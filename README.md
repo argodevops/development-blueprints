df001547c4688baf03f31d902e06acf42eb0c965
ryan.b <ryan.b@argodevops.co.uk>
2023-05-04 11:09:41 +0000

    docs: semantic-release configurations

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # development-blueprints
+
 An Argo wide repo for development templates and standards we use as blueprints across projects
+
+- [Semantic-release](./semantic-release-configs/README.md)
---
+++ semantic-release-configs/js/npm.release.config.js
@@ -0,0 +1,43 @@
+const pkg = require("./package.json");
+
+const branches = [
+  "master",
+  {
+    name: "alpha",
+    prerelease: true,
+  },
+];
+
+const commitAnalyzer = "@semantic-release/commit-analyzer";
+const releaseNotesGenerator = "@semantic-release/release-notes-generator";
+const changelog = "@semantic-release/changelog";
+const npm = "@semantic-release/npm";
+
+const deprecate = [
+  "semantic-release-npm-deprecate",
+  {
+    deprecations: [
+      {
+        version: "< ${nextRelease.version.split('.')[0]}",
+        message: "Please use ^${nextRelease.version.split('.')[0]}.0.0.",
+      },
+    ],
+  },
+];
+
+const git = "@semantic-release/git";
+
+const teams = ["@argodevops/semantic-release-teams", { packageName: pkg.name }];
+
+module.exports = {
+  branches,
+  plugins: [
+    commitAnalyzer,
+    releaseNotesGenerator,
+    changelog,
+    npm,
+    deprecate,
+    git,
+    teams,
+  ],
+};
